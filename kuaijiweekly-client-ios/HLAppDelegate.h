ea07f4657c1fdfc3d3189777010c175ebd808cfa
huluo <huluo-tech@sunlands.com>
2014-05-26 10:49:53 +0800

    Initial Commit

---
+++ kuaijiweekly-client-ios/HLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HLAppDelegate.h
+//  kuaijiweekly-client-ios
+//
+//  Created by Terry on 5/26/14.
+//  Copyright (c) 2014 com.huluo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ kuaijiweekly-client-ios/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  kuaijiweekly-client-ios
+//
+//  Created by Terry on 5/26/14.
+//  Copyright (c) 2014 com.huluo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HLAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HLAppDelegate class]));
+    }
+}
